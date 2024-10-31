490db96da4d371a4e11920db59ad8e3c84f3605b
ouxionghu365 <1650379513@qq.com>
2024-10-31 15:04:05 +0800

    firstday

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"coredemo/framework"
+	"net/http"
+)
+
+func main() {
+	server := &http.Server{
+		Handler: framework.NewCore(),
+		Addr:    "localhost:8080",
+	}
+
+	server.ListenAndServe()
+}
